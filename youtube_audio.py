0f9bf950327b17213214c58a9a0df92f75973333
nishantprime <nishantkumarprime@gmail.com>
2025-10-28 16:27:29 +0530

    Update youtube_audio.py

--- youtube_audio.py
+++ youtube_audio.py
@@ -8,24 +8,28 @@ import json
 query = st.text_input('enter song name', 'extreme ways')
 
 search = SearchVideos(query, offset=1, mode="json", max_results=1)
-results_data = json.loads(search.result())
+results = search.result()
+
 
 try:
-    if results_data and results_data['search_result']:
-        url = results_data['search_result'][0]['link']
-        ydl_opts = {
-                'format': 'bestaudio/best',
-                'quiet': True,
-                'noprogress': True,
-                'nocheckcertificate': True
-            }
-        info = yt_dlp.YoutubeDL(ydl_opts).extract_info(url, download=False)
-        
-        info['fulltitle'] , info['duration_string']
-        st.image(info['thumbnail'])
-        
-        st.audio(info['url'])
-    
+    if results:
+        results_data = json.loads()
+        if results_data and results_data['search_result']:
+            url = results_data['search_result'][0]['link']
+            ydl_opts = {
+                    'format': 'bestaudio/best',
+                    'quiet': True,
+                    'noprogress': True,
+                    'nocheckcertificate': True
+                }
+            info = yt_dlp.YoutubeDL(ydl_opts).extract_info(url, download=False)
+            
+            info['fulltitle'] , info['duration_string']
+            st.image(info['thumbnail'])
+            
+            st.audio(info['url'])
+        else:
+            "No results found for the query."
     else:
         "No results found for the query."
 except:
