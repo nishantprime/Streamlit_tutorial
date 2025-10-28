b452fc87932b1b37a9739f55ab4f35254d3388f0
nishantprime <nishantkumarprime@gmail.com>
2025-10-28 16:31:23 +0530

    Update youtube_audio.py

--- youtube_audio.py
+++ youtube_audio.py
@@ -11,27 +11,29 @@ search = SearchVideos(query, offset=1, mode="json", max_results=1)
 results = search.result()
 
 
-
-if results:
-    results_data = json.loads()
-    if results_data and results_data['search_result']:
-        url = results_data['search_result'][0]['link']
-        ydl_opts = {
-                'format': 'bestaudio/best',
-                'quiet': True,
-                'noprogress': True,
-                'nocheckcertificate': True
-            }
-        info = yt_dlp.YoutubeDL(ydl_opts).extract_info(url, download=False)
-        
-        info['fulltitle'] , info['duration_string']
-        st.image(info['thumbnail'])
-        
-        st.audio(info['url'])
+try:
+    if results:
+        results_data = json.loads(results)
+        if results_data and results_data['search_result']:
+            url = results_data['search_result'][0]['link']
+            ydl_opts = {
+                    'format': 'bestaudio/best',
+                    'quiet': True,
+                    'noprogress': True,
+                    'nocheckcertificate': True
+                }
+            info = yt_dlp.YoutubeDL(ydl_opts).extract_info(url, download=False)
+            
+            info['fulltitle'] , info['duration_string']
+            st.image(info['thumbnail'])
+            
+            st.audio(info['url'])
+        else:
+            "No results found for the query."
     else:
         "No results found for the query."
-else:
-    "No results found for the query."
+except:
+    "Could not fetch the song. Possible copyright"
 
 
 
